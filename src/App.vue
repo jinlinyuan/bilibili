b13bee2b4d6345b68af08e06b8aaf65ec33d7b58
yuanlin.jin <yuanlin.jin@sinocare.com>
2020-10-18 09:27:46 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- 保持页面应用生命周期 -->
+    <keep-alive>
+      <router-view v-if="$route.meta.keepalive" />
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepalive"></router-view>
+    <!-- <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div> -->
+    <div id="main" class="content"></div>
+  </div>
+</template>
+
+<style lang="less">
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #f4f4f4;
+}
+p {
+  margin: 0;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 页面UI -->
+  <!-- 只能存在唯一一个渲染的根元素 -->
+  <div></div>
+</template>
+
+<script>
+// js
+export default {
+  // 组件加载成功
+  created() {},
+  // 组件渲染成功
+  mounted() {},
+  // 组件销毁成功
+  destroyed() {},
+  // 定义数据
+  data() {
+    return {};
+  },
+  // 方法
+  methods: {},
+  // 页面中所使用的的组件列表
+  components: {},
+  // 计算属性
+  computed: {},
+  // 监听器
+  watch: {
+    // newValue oldValue
+  },
+};
+</script>
+
+// 样式
+<style lang="less"></style>
